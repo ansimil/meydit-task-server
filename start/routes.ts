5875690e6efa75a8bdcf7f8afd0850b1773c94a7
John Doe <andrewsimonmiller@gmail.com>
2023-04-13 10:32:05 +1000

    added basic route for signup

--- start/routes.ts
+++ start/routes.ts
@@ -21,6 +21,8 @@ import Database from '@ioc:Adonis/Lucid/Database'
 import Route from '@ioc:Adonis/Core/Route'
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import { v4 as uuid } from 'uuid';
+import Hash from '@ioc:Adonis/Core/Hash';
+import { DateTime } from 'luxon'
 
 Route.get('/proposals', async () => {
   return await Database
@@ -62,6 +64,41 @@ Route.post('/add-proposal', async ({request, response}) => {
   }
 
   
+})
+
+Route.post('/signup', async ({ request, response }) => {
+  const { email, firstName, surname, password } = request.body()
+  const hashedPassword = await Hash.make(password)
+  const userId = uuid()
+  const existingUser = await  Database
+  .from('users')
+  .where('email_address', email)
+
+  if (existingUser.length > 0){
+    response.unauthorized("user already exists")
+    return
+  }
+  
+  const user = {
+    "first_name": firstName,
+    "surname": surname,
+    "email_address": email,
+    "password": hashedPassword,
+    "user_id": userId,
+    "proposals": [],
+    "jobs": [],
+    "created_at": DateTime.now()
+  }
+  try {
+    await Database
+    .table('users')
+    .insert({...user})
+    response.ok("registration successful")
+  }
+  catch(error) {
+    console.log(error.messages)
+    response.badRequest(error.messages)
+  }
 })
 // .middleware(async (ctx, next) => {
   
